730e150c74fcfdb9ebc7429c33e7bf1011864460
Rafael Miralha <rafamiralha1@gmail.com>
2023-07-22 12:10:00 -0300

    como comentar em JS e Tipos de Dados

---
+++ Rocket/JavaScript/comentários.js
@@ -0,0 +1,6 @@
+console.log("bem vindos ao starter!!!")
+//comentário de linha 
+
+/* COMENTARIO 
+COM 
+MAIS E UMA LINHA*/
--- Rocket/JavaScript/script.js
+++ Rocket/JavaScript/script.js
@@ -1,1 +1,26 @@
-console.log("bem vindos ao starter!!!")
+//tipos de dados
+
+console.log(" Tipo String")//exemplo de string
+console.log(33)//exemplo de number
+console.log(12.5)//exemplo de float
+console.log(12/"!")//Quando no console é feito um calculo com valor invalido ele informa NaN (Not a Number) indicando que algum valor da expressão é invalido
+console.log(10 === Infinity)// temos como declarar "infinito" como parâmetro, pouco usado mas é possível
+console.log(false)
+console.log(true)
+// exemplo de declaração de dados Booleanos
+console.log(null)
+console.log(undefined)
+//declarar um objeto nulo é diferente de declarar um objeto indefinido, eles não são a mesma coisa.
+console.log({
+    name:"josé",
+    idade:20,
+    falar:function(){
+      console.log("andar")  
+    }
+})
+/*declaração de objetos funciona declarando cada tipo de dado separando por virgula e no caso de métodos(funções) utilizamos functio(){
+
+} dentro das chaves é feita a declaração do que a função faz.
+
+ */
+console.log(["josé",20])//declaração de arrays 
