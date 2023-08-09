b631f92e18baf3f5ebc649aa907de67acc124e4b
Rafael Miralha <rafamiralha1@gmail.com>
2023-08-09 17:41:05 -0300

    Como criar um object e um Array

---
+++ Rocket/JavaScript/object.js
@@ -0,0 +1,18 @@
+//Object 
+let person =
+{
+    
+    name:'Rafa',
+    age: 22
+
+}
+console.log('Seu nome é ' +person.name+' e sua idade é '+ person.age)
+
+//Array
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat'
+]
+console.log(animals[2])
+// Preferível guardar só um tipo de valor por Array para não causat confusão
