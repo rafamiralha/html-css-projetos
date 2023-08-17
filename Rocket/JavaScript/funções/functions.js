00b34c8da9bd57ab059014171c5e90fd2bf40a9d
Rafael Miralha <rafamiralha1@gmail.com>
2023-08-17 14:03:13 -0300

    funcionamento de função

---
+++ Rocket/JavaScript/funções/functions.js
@@ -0,0 +1,15 @@
+//aplicativo de frases motivacionais 
+
+
+//declarando uma função
+//function statement 
+function createPhrases() {
+    console.log('Estudar é muito bom')
+console.log('Paciência e persistência')
+console.log('Revisão é a mãe do aprendizado')    
+}
+
+//chamar uma função
+//call a function
+createPhrases();
+createPhrases(); 
