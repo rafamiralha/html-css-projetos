ef54d3baae82fcb8d7f12b6e86f92ea5c8c1e794
Rafael Miralha <rafamiralha1@gmail.com>
2024-03-18 19:26:59 -0300

    finalização do projeto de cartão de contato

---
+++ Rocket/project/Untitled-1.js
@@ -0,0 +1,14 @@
+let unknown = "<div><div>";
+// const checkedDiv = /"</div>"/;
+function checkDiv(String) {
+    for (let i = 0; i < unknown.length; i++){
+        if (unknown = /<div>/){
+            if(unknown = /<div>/){
+                unknown[i].replace(unknown, "</div>");
+                
+            }
+        }
+        unknown+=unknownChecked;
+    }
+}return unknownChecked
+console.log(unknownChecked);
--- Rocket/project/script.js
+++ Rocket/project/script.js
@@ -1,21 +1,36 @@
-function toggleMode(){
-const body = document.documentElement
-body.classList.toggle("light")
+// function toggleMode(){
+// const body = document.documentElement
+// body.classList.toggle("light")
 
-// how a toggle works 
+// // how a toggle works 
+// // if (body.classList.contains('light')){
+// //     body.classList.remove("light")    
+// // } else{
+// //     body.classList.add("light")
+// // }
+// const img = document.querySelector("#profile img")
 // if (body.classList.contains('light')){
-//     body.classList.remove("light")    
-// } else{
-//     body.classList.add("light")
+// img.setAttribute('src', './assets/avatar-lightmode.png')
+// img.setAttribute('alt','Imagem de fundo com homem fone de ouvido branco sorrindo')
+// }else{
+// img.setAttribute('src','./assets/avatar1.png')
+// img.setAttribute('alt','Imagem de fundo de uma praça ilumindada de noite com homem olhando para o lado')
 // }
-const img = document.querySelector("#profile img")
-if (body.classList.contains('light')){
-img.setAttribute('src', './assets/avatar-lightmode.png')
-img.setAttribute('alt','Imagem de fundo com homem fone de ouvido branco sorrindo')
-}else{
-img.setAttribute('src','./assets/avatar1.png')
-img.setAttribute('alt','Imagem de fundo de uma praça ilumindada de noite com homem olhando para o lado')
-}
-//how I changed the avatar img
+// //how I changed the avatar img
 
-}
+// }
+
+let unknown = "<div><div>";
+// const checkedDiv = /"</div>"/;
+function checkDiv(String) {
+    for (let i = 0; i < unknown.length; i++){
+        if (unknown = /<div>/){
+            if(unknown = /<div>/){
+                unknown[i].replace(unknown, "</div>");
+                
+            }
+        }
+        unknown+=unknownChecked;
+    }
+}return unknownChecked
+console.log(unknownChecked);
